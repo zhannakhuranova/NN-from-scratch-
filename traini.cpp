e7a198010b61b0123486118c5268b329971d21a2
Zhanna Khuranova <47778811+zhannakhuranova@users.noreply.github.com>
2026-01-10 00:59:03 +0100

    Update traini.cpp

--- traini.cpp
+++ traini.cpp
@@ -8,7 +8,7 @@ int main()
 
 	int n;
 
-	std::string filename  = ("/home/pidhii/t/a.txt");
+	std::string filename  = ("$HOME$/a.txt");
 	std::vector <int> datafile;
 	std :: ifstream in (filename);
 
@@ -40,19 +40,16 @@ int main()
 		}
 	}
 
-
 	if (choi == 1)
 	{
 		for (int f = 0; f < sz; f++) {
-			inpu.push_back(datafil[f]);
+			inpu.push_back(datafile[f]);
 		}
 	}
-
 
 	//couvctr(datafile);
 	datafile.erase( datafile.begin(), datafile.begin() + sz);
 	couvctr(datafile); // cout marix of the data
-
 
 	std::vector <double> wei;
 
@@ -60,12 +57,10 @@ int main()
 		wei.push_back(rannum(-4.0, 4.0));
 	}
 
-
-	double neuro;
+	double neuro=0.0;
 	for ( int i = 0; i < sz; i++) {
 		neuro += inpu[i] * wei[i];
 	}
-
 
 	double pred_da; // predicted data
 	int ki = 0;
@@ -77,17 +72,15 @@ int main()
 
 			if (ki == sz) {
 				pred_da = datafile[i + 1];
+					
 			}
 		}
 	}
-
-
 
 	std::cout << " Predicted data is... " << pred_da << std::endl;
 
 	std::vector <double> losfu;
 	std::vector <double> neurov; // vector of neurons
-
 
 	neurov.push_back(neuro);
 
@@ -95,10 +88,8 @@ int main()
 	out.open("hello.dat");
 	out << "Weigh" << "				" << "lossfuction" << std::endl;
 
-
 	double neu;
-
-
+	
 	for (int i = 0; i < sz * ite; i++)
 	{
 
@@ -107,18 +98,15 @@ int main()
 			actfu(neurov.at(i));
 			losfu.push_back( 2 * lr * (pred_da - actfu(neurov.at(i))) * (actfu(neurov.at(i))) * (1 - actfu(neurov.at(i))));
 			wei.at(j) += losfu.at(i);
-			neuro = + wei.at(j) + inpu.at(j);
+			neuro +=  wei.at(j) + inpu.at(j);
 			out << wei.at(j) << "			" << losfu.at(i) << std::endl;
 			//couvctr(wei);
 		}
-
-
 
 		neurov.push_back(neuro);
 		neuro = 0;
 
 		out << std :: endl;
-
 	}
 
 }
