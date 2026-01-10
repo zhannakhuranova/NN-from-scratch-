99f9b385c9cbe38015a5c492c2b6ef1eab1f8960
Zhanna Khuranova <47778811+zhannakhuranova@users.noreply.github.com>
2026-01-10 22:32:56 +0100

    Add files via upload

--- traini.cpp
+++ traini.cpp
@@ -1,113 +1,126 @@
 
 #include "traini.h"
-
+#include <cassert>
 
 int main()
 {
 	srand ((unsigned) time( NULL) ); //for rand
 
-	int n=0;
-    
-	std::string filename  = ("$HOME$/a.txt"); // read classification approaches
-	if(filename) // by default you have a file with classification data
-	{
-    	std::vector <int> datafile;
-		std :: ifstream file (filename);
-	
+	int n = 0;
+
+	std::string filename  = ("/Users/zhannabananna/Desktop/neuro/a.txt"); // read classification approaches
+
+    std::vector <int> datafile;
+		std:: ifstream file (filename);
+
 		while (file >> n) {
 			datafile.push_back(n);
 		}
-	}
+
+  couvctr(datafile,"datafile");  // cout function in header for vector within the name print
+
 	file.close();
-	couvctr(datafile);  //
-// sz - size of input data vector 
+
+
+// sz - size of input data vector
 	int sz = 0;
 	std::cout << "enter size of array " ;
 	std::cin >> sz; // size here corresponding to data type is be only 3, but you could change a.txt file for your data size
+  assert(sz > 0 && "Vector size size must be greater than zero.");
 
-	int choi=0;
+	int choi = 0;
 	std::cout << "( Read data from terminal or from file ) ? (0 : 1)" << std::endl; // 0 from terminal
 	std::cin >> choi;
-	//feed the data 
-	std::cout << "enter array { " ;
-	std::cout << "}  " ;
-	int valu;
-	std::vector <double> inpu;
+  assert(choi ==  0 || choi == 1  && " Choi coresponding to the 1 or 0");
+	//insert the data
+	int valu = 0;
+	std::vector <double> input;
 	if ( choi == 0 )//0 from terminal
 	{
+		std::cout << "enter array { " ;
 		for ( int i = 0; i < sz; i++)
-		{	std::cin >> valu;
-			inpu.push_back(valu);
-		 	assert(inpu.size() > sz && "Vector size must be smaller than input size.");
-		}
+			 {	std::cin >> valu;
+				// if(valu)
+		  	input.push_back(valu);
+				assert(valu ==  0 || valu == 1  && "Input data coresponding to the 1 or 0");
+				assert(input.size() <= sz  && "Vector size size must sz.");
+           if(input.size()==sz){ std::cout << "}" ;};
+	    	}
+
 	}
 
-	if (choi == 1) //1 from file
+	if (choi == 1) //1 - read from the file
 	{
 		for (int i = 0; i < sz; i++) {
-			inpu.push_back(datafile[i]);
+			input.push_back(datafile[i]);
 		}
 	}
 
 	//couvctr(datafile);
-	datafile.erase( datafile.begin(), datafile.begin() + sz); // 
-	couvctr(datafile); // cout marix of the data
+	datafile.erase(datafile.begin(), datafile.begin() + sz); //
+  //couvctr(datafile); // cout marix of the data
 
 	std::vector <double> wei;
-// tread weigh vector with random values in a range -4:4 because of sigmoid activation function 
+ // treat weigh vector with random values in a range -4:4 (because of sigmoid activation function)
 	for (int t = 0; t < sz; t++) {
-		wei.push_back(rannum(-4.0, 4.0));
-	}
-//treat neuron with inputdata and weigh
-	double neuro=0.0;
-	for ( int i = 0; i < sz; i++) {
-		neuro += inpu[i] * wei[i];
-	}
-//read data classification predicted data pred_da
-	double pred_da; // predicted data
-	int ki = 0;
+		wei.push_back(rannum(-4.0, 4.0));	}
 
+//treat neuron with inputdata*weigh
+	double neuro = 0.0;
+	for ( int i = 0; i < sz; i++) {
+		neuro += input[i] * wei[i];}
+		std::vector <double> neurov;
+		neurov.push_back(neuro);
+
+//
+// example 0001 -> 000 predicted data corresponds to label 1
+	double pred_data; // predicted data
+	v pred_data_vec; // predicted data
+	int ki = 0; //datafile iterator
 	for (int i = 0; i < datafile.size(); i++) {
-		if (inpu[i] == datafile.at(i)) {
+		if (input[i] == datafile.at(i)) {
 			ki++;
-			if (ki == sz && i+1< datafile.size()) {
-				pred_da = datafile[i + 1];
+			  if (ki == sz && i+1 < datafile.size()) { // if ki as much as a vector size create pred_data
+				   pred_data= datafile[i + 1];
+					 pred_data_vec.push_back(pred_data);
 			}
 		}
 	}
+	std::cout << " Predicted data is... " << pred_data << std::endl;
 
-	std::cout << " Predicted data is... " << pred_da << std::endl;
-//create loss function and neuro vector neurov
+//create loss function and neurons vector
 	std::vector <double> losfu;
-	std::vector <double> neurov; // vector of neurons
 
-	neurov.push_back(neuro);
-//creat and write results in a hello.dat file 
+//create and write results in a hello.cvs file
 	std::ofstream out;
-	out.open("hello.dat");
-	out << "Weigh" << "				" << "lossfuction" << std::endl;
+	out.open("hello.csv");
+	out << "Weigh"<<"			    	"<<"actual_loss"<<"       "<<"lossfunction"<< std::endl;
 
-	double neu;
-	//ite is a number of iteration in a .h file
-	
-	for (int i = 0; i < sz * ite; i++)
-	{
-
+  double neu = 0.0;
+  double predictions = 0.0;
+  double actual_loss = 0.0;
+	v actual_loss_vec;
+////iter - number of iteration
+//epoch sz*iter
+	for (int i = 0; i < sz * iter; i++){
 		for ( int j = 0; j < sz; j++) {
 
-			actfu(neurov.at(i));
-			losfu.push_back( 2 * lr * (pred_da - actfu(neurov.at(i))) * (actfu(neurov.at(i))) * (1 - actfu(neurov.at(i))));
-			wei.at(j) += losfu.at(i);
-			neuro +=  wei.at(j) + inpu.at(j);
-			out << wei.at(j) << "			" << losfu.at(i) << std::endl;
-			//couvctr(wei);
+			actfu(neurov.at(i)); //treat activation function with neuron value
+			predictions =	actfu( neurov.at(i) );
+			actual_loss = pow( pred_data - predictions,2 );// loss function
+			actual_loss_vec.push_back(actual_loss);
+			losfu.push_back( 2 * lr * ( pred_data - actfu(neurov.at(i))) * (actfu(neurov.at(i))) * (1 - actfu(neurov.at(i))) );//gradient
+			wei.at(j) += losfu.at(i); //weight changes
+			neuro +=  wei.at(j) + input.at(j); // neu neuron
+			out << wei.at(j) << "," << actual_loss << "," << losfu.at(i) << std::endl;
 		}
 
-		neurov.push_back(neuro);
-		neuro = 0;
-
+		neurov.push_back(neuro); //vector of neuron
+		neuro = 0; //clean neuro var
 		out << std :: endl;
+
 	}
-out.clode();
+couvctrow( actual_loss_vec, "actual_loss_vec");
+out.close();
 return 0;
 }
